769d5bf30e542cf4064e2322a18f937a21379f8e
Shintaro Ishida <sh1sh1dax@gmail.com>
2017-10-29 22:09:18 +0900

    Remove unnecessary code

--- main.go
+++ main.go
@@ -17,14 +17,12 @@ func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello, %q\n", html.EscapeString(r.URL.Path))
 		db, err := sql.Open("postgres", dbURL)
-		fmt.Printf("%v\n", db)
 		if err != nil {
 			log.Fatal("failed to connect to db")
 		}
 		defer db.Close()
 
 		rows, err := db.Query("select * from test;")
-		fmt.Printf("%v\n", rows)
 		if err != nil {
 			log.Fatal("db select error")
 		}
