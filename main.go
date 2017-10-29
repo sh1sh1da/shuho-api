622aaf4b29a6af94f5967b4d7f61e424bd8fed0c
Shintaro Ishida <sh1sh1dax@gmail.com>
2017-10-29 13:56:18 +0900

    Update for deployment

--- main.go
+++ main.go
@@ -1,11 +1,11 @@
 package main
 
 import (
-	"database/sql"
+	// "database/sql"
 	"fmt"
-	_ "github.com/lib/pq"
+	// _ "github.com/lib/pq"
 	"html"
-	"log"
+	// "log"
 	"net/http"
 	"os"
 )
@@ -15,31 +15,30 @@ func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello, %q\n", html.EscapeString(r.URL.Path))
 
-		db, err := sql.Open("postgres", "user=testrole password=testrole dbname=test sslmode=disable")
-		fmt.Printf("%v\n", db)
-		if err != nil {
-			fmt.Println("failed to connect to db")
-		}
-		defer db.Close()
+		// db, err := sql.Open("postgres", "user=testrole password=testrole dbname=test sslmode=disable")
+		// fmt.Printf("%v\n", db)
+		// if err != nil {
+		// 	fmt.Println("failed to connect to db")
+		// }
+		// defer db.Close()
 
-		rows, err := db.Query("select * from test;")
-		fmt.Printf("%v\n", rows)
-		if err != nil {
-			fmt.Println("db select error")
-		}
+		// rows, err := db.Query("select * from test;")
+		// fmt.Printf("%v\n", rows)
+		// if err != nil {
+		// 	fmt.Println("db select error")
+		// }
 
-		for rows.Next() {
-			var id int
-			var value int
-			err := rows.Scan(&id, &value)
-			if err != nil {
-				//log.Fatal("scan error: %v", err)
-			}
-			//fmt.Printf("id: %d, value: %d\n", id, value)
-			//fmt.Fprintf(w, "id: %d, value: %d\n", id, value)
-		}
+		// for rows.Next() {
+		// 	var id int
+		// 	var value int
+		// 	err := rows.Scan(&id, &value)
+		// 	if err != nil {
+		// 		//log.Fatal("scan error: %v", err)
+		// 	}
+		// 	//fmt.Printf("id: %d, value: %d\n", id, value)
+		// 	//fmt.Fprintf(w, "id: %d, value: %d\n", id, value)
+		// }
 	})
-	http.ListenAndServe(":"+port, nil)
-
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	http.ListenAndServe(":" + port, nil)
+	// log.Fatal(http.ListenAndServe(":" + port, nil))
 }
