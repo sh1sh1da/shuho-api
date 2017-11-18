d1b8d5f2285b6116c5efaf3127f4d6d0060d47bf
Shintaro Ishida <sh1sh1dax@gmail.com>
2017-11-18 20:26:36 +0900

    Add users endpoint

--- main.go
+++ main.go
@@ -13,14 +13,14 @@ import (
 func main() {
 	port := os.Getenv("PORT")
 	dbURL := os.Getenv("DATABASE_URL")
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		log.Fatal("failed to connect to db")
+	}
+	defer db.Close()
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello, %q\n", html.EscapeString(r.URL.Path))
-		db, err := sql.Open("postgres", dbURL)
-		if err != nil {
-			log.Fatal("failed to connect to db")
-		}
-		defer db.Close()
 
 		rows, err := db.Query("select * from test;")
 		if err != nil {
@@ -37,5 +37,24 @@ func main() {
 			fmt.Fprintf(w, "id: %d, value: %d\n", id, value)
 		}
 	})
+
+	http.HandleFunc("/users", func(w http.ResponseWriter, r *http.Request) {
+		query := `select * from "user"`
+		rows, err := db.Query(query)
+		if err != nil {
+			log.Fatalf("unable to select from db. err: %v", err);
+		}
+
+		for rows.Next() {
+			var id int
+			var name string
+			err := rows.Scan(&id, &name)
+			if err != nil {
+				log.Fatal("scan error: %v", err)
+			}
+			fmt.Fprintf(w, "id: %d, name: %s\n", id, name)
+		}
+	})
+
 	log.Fatal(http.ListenAndServe(":"+port, nil))
 }
